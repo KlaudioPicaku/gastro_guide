446b3b7867c0c497f945f07698aeb1363c9d165d
Klaudio Picaku <picaku.klaudio@yahoo.com>
2023-05-29 17:57:06 +0200

    first commit

---
+++ src/main/java/com/uniroma3/it/gastroguide/GastroguideApplication.java
@@ -0,0 +1,13 @@
+package com.uniroma3.it.gastroguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniroma3/it/gastroguide/GastroguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniroma3.it.gastroguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
